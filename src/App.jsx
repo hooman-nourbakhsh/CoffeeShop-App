674e95a07ffb100a87ad5ad95953c8aa1d54e64e
HooMaN <eng.homan@gmail.com>
2025-05-13 20:38:51 +0330

    build: :wrench: Add aos and slick-carousel dependencies and configure AOS
    I installed aos, react-slick, and slick-carousel via npm. In src/App.jsx, I imported and initialized AOS with custom settings using useEffect. In src/main.jsx, I imported slick-carousel CSS files to prepare for carousels.

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,18 @@
+import { useEffect } from "react";
 import Navbar from "./components/Navbar";
+import Aos from "aos";
+import "aos/dist/aos.css";
 
 const App = () => {
+  useEffect(() => {
+    Aos.init({
+      offset: 100,
+      duration: 700,
+      easing: "ease-in",
+      delay: 100,
+    });
+  });
+
   return (
     <div>
       <Navbar />
